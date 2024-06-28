b46eb44712c1562720fe8ebeb24bac157493c9b8
nodyalclaydon <72510779+nodyalclaydon@users.noreply.github.com>
2024-06-27 21:52:51 -0500

    remove htmx, update header component

--- components/header.js
+++ components/header.js
@@ -1,27 +1,46 @@
-const headerEl = document.createElement( "template" );
+// const headerEl = document.createElement( "template" );
 
-headerEl.innerHTML = `
-    <style>
-        div {
-            background-color: #666666;
-            padding: 1rem;
-            text-align: center;
-            color: #efefef;
-            width: 100%;
-        }
-    </style>
-    <div>Hello, world!!! This is a reusable web component</div>
-`;
+// headerEl.innerHTML = `
+//     <style>
+//         div {
+//             background-color: #666666;
+//             padding: 1rem;
+//             text-align: center;
+//             color: #efefef;
+//             width: 100%;
+//         }
+//     </style>
+//     <div>Hello, world!!! This is a reusable web component</div>
+// `;
 
 class Header extends HTMLElement {
     constructor() {
         super();
+        this.navItems = "";
+    }
+
+    static get observedAttributes() {
+        return [ "data" ];
+    }
+
+    attributeChangedCallback( attr, oldValue, newValue ) {
+        this.dataArray = JSON.parse( newValue );
+        this.dataArray.forEach( el => {
+            this.navItems += `<a href="${ el.link }" class="nav-item ${ el.active ? "active" : ""}">${ el.label }</a>`;
+        });
     }
 
     connectedCallback() {
-        const shadowRoot = this.attachShadow({ mode: "closed" });
+        // const shadowRoot = this.attachShadow({ mode: "closed" });
 
-        shadowRoot.appendChild( headerEl.content );
+        // shadowRoot.appendChild( headerEl.content );
+        this.innerHTML = `
+            <nav class="nav">
+                <div class="nav-items">
+                    ${ this.navItems }
+                </div>
+            </nav>
+        `;
     }
 }
 
